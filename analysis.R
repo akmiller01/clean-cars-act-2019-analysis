77160ed7b09fa08798af3e392cbe5edc23581fdb
Alex Miller <alex.k.miller@gmail.com>
2019-06-25 08:59:03 -0400

    New map

--- analysis.R
+++ analysis.R
@@ -14,20 +14,29 @@ dat_march = fread("data/MVA_Electric_and_Hybrid_Vehicle_Registrations_by_County_
 setnames(dat_march,"Count","March")
 dat_april = fread("data/MVA_Electric_and_Hybrid_Vehicle_Registrations_by_County_as_of_April_2019.csv")
 setnames(dat_april,"Count","April")
+dat_may = fread("data/MVA_Electric_and_Hybrid_Vehicle_Registrations_by_County_as_of_May_2019.csv")
+setnames(dat_may,"Count","May")
 dat = merge(dat_march,dat_april)
-dat$diff_count = dat$April-dat$March
+dat = merge(dat,dat_may)
+dat$diff_count_1 = dat$April-dat$March
+dat$diff_count_2 = dat$May-dat$April
+dat$diff_count = dat$May-dat$March
 
 march_tot = sum(dat$March)
 message("March 2019 ZEV total: ", march_tot)
 april_tot = sum(dat$April)
 message("April 2019 ZEV total: ", april_tot)
-growth_rate = sum(dat$diff_count)
-message("Monthly ZEV growth rate: ", growth_rate)
+may_tot = sum(dat$May)
+message("May 2019 ZEV total: ", may_tot)
+growth_rate_1 = sum(dat$diff_count_1)
+message("Monthly ZEV growth rate (March to April): ", growth_rate_1)
+growth_rate_2 = sum(dat$diff_count_2)
+message("Monthly ZEV growth rate (April to May): ", growth_rate_2)
 
-months_until_jan_2020 = 10
-months_until_jan_2025 = 70
-message("Estimated January 2020 ZEV total: ", april_tot+(growth_rate*months_until_jan_2020))
-message("Estimated January 2025 ZEV total: ", april_tot+(growth_rate*months_until_jan_2025))
+months_until_jan_2020 = 7
+months_until_jan_2025 = 7+(5*12)
+message("Estimated January 2020 ZEV total: ", may_tot+(growth_rate_2*months_until_jan_2020))
+message("Estimated January 2025 ZEV total: ", may_tot+(growth_rate_2*months_until_jan_2025))
 
 # Harmonize county names
 simpleCap <- function(x) {
@@ -44,10 +53,17 @@ dat$County[which(dat$County=="Queen Annes")] = "Queen Anne's"
 dat$County[which(dat$County=="Saint Marys")] = "St. Mary's"
 
 setnames(counties@data, "COUNTY", "County")
-dat_tab = data.table(dat)[,.(March=sum(March),April=sum(April),diff_count=sum(diff_count)),by=.(County)]
+dat_tab = data.table(dat)[,.(
+  March=sum(March),
+  April=sum(April),
+  May=sum(May),
+  diff_count_1=sum(diff_count_1),
+  diff_count_2=sum(diff_count_2),
+  diff_count=sum(diff_count)
+  ),by=.(County)]
 
 # Quick bar plot
-dat_long = melt(dat_tab,id.vars="County",measure.vars=c("March","April"),variable.name="Month")
+dat_long = melt(dat_tab,id.vars="County",measure.vars=c("March","April","May"),variable.name="Month")
 dat_long_order = subset(dat_long,Month=="March")
 dat_long_order = dat_long_order[order(-dat_long_order$value),]
 dat_long$County = factor(dat_long$County, levels = dat_long_order$County)
@@ -69,9 +85,9 @@ diff.pal = c("#d7191c", "#fdae61", "#ffffbf", "#abd9e9", "#2c7bb6")
 diff.palbins = c(0, 2, 10, 40, 150)
 names(diff.palbins)=c("≤ 0", "2", "10", "40", "≥ 150")
 
-april.pal = c("#ffffcc", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494")
-april.palbins = c(50, 100, 250, 1000, 5000)
-names(april.palbins)=c("≤ 50", "100", "250", "1000", "≥ 5000")
+may.pal = c("#ffffcc", "#a1dab4", "#41b6c4", "#2c7fb8", "#253494")
+may.palbins = c(50, 100, 250, 1000, 5000)
+names(may.palbins)=c("≤ 50", "100", "250", "1000", "≥ 5000")
 
 
 diff_map = 
@@ -94,22 +110,22 @@ diff_map =
 
 ggsave("output/diff_map.png",diff_map,height=3,width=7)
 
-april_map = 
+may_map = 
   ggplot(counties.f)+
-  geom_polygon( aes(x=long,y=lat,group=group,fill=April,color="#ffffff",size=0.21))+
+  geom_polygon( aes(x=long,y=lat,group=group,fill=May,color="#ffffff",size=0.21))+
   scale_color_identity()+
   scale_size_identity()+
   coord_fixed(1) +
   scale_fill_gradientn(
     na.value="#d0cccf",
     guide="legend",
-    breaks=april.palbins,
-    colors=april.pal,
-    values=rescale(april.palbins)
+    breaks=may.palbins,
+    colors=may.pal,
+    values=rescale(may.palbins)
   ) +
   expand_limits(x=counties.f$long,y=counties.f$lat)+
   theme_classic()+
   theme(axis.line = element_blank(),axis.text=element_blank(),axis.ticks = element_blank())+
-  labs(x="",y="",fill="Active ZEV registrations\nas of April 2019")
+  labs(x="",y="",fill="Active ZEV registrations\nas of May 2019")
 
-ggsave("output/april_map.png",april_map,height=3,width=7)
+ggsave("output/may_map.png",may_map,height=3,width=7)
